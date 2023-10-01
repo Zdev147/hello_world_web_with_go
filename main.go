29ea889bbe40b9c3a5c344942c4a59009b6e7886
Ahmed <ahmed.flutter@magnatectsystems.com>
2023-10-01 13:06:07 +0500

    created another route and formatted some code

--- main.go
+++ main.go
@@ -5,16 +5,35 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8000"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	// Fprintln is used to print on web
+	// _ is used to ignore a value, as go require a variable to use if it is declared
+	_, err := fmt.Fprintln(w, "This home page")
+
+	if err != nil {
+		// Println is used to print on console
+		fmt.Println(err)
+	}
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintln(w, "This about page")
+
+	if err != nil {
+		// Println is used to print on console
+		fmt.Println(err)
+	}
+}
+
+// main is the entry point of go program
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		bytes, err := fmt.Fprintf(w, "Hello world!")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Printf("Number of bytes printed: %d", bytes)
-	})
-
-	http.ListenAndServe(":8000", nil)
+	fmt.Printf("Starting server on port %s", portNumber)
+	http.ListenAndServe(portNumber, nil)
 }
