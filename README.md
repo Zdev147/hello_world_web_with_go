800ea162ab935588954c8fff24e2adb21cceac4d
Ahmed <ahmed.flutter@magnatectsystems.com>
2023-10-01 12:53:40 +0500

    started a server, created a home route and printed hello world on that route

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A simple website using go
 ## Description
 
 Exploring different aspects of go
+
+At this point, I'm using "go run main.go" command to run the program, as there is only one go file to compile
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		bytes, err := fmt.Fprintf(w, "Hello world!")
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Printf("Number of bytes printed: %d", bytes)
+	})
+
+	http.ListenAndServe(":8000", nil)
 }
