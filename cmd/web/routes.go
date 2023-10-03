5aaf909ee0e9f80acc1e0d7aeb06deedcf54d381
Ahmed <ahmed.flutter@magnatectsystems.com>
2023-10-03 11:44:58 +0500

    removed pat and added chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,30 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/zdev147/hello_world_web_with_go/pkg/config"
 	"github.com/zdev147/hello_world_web_with_go/pkg/handler"
 )
 
+// // routes with pat
+// func routes(app *config.AppConfig) http.Handler {
+// 	router := pat.New()
+
+// 	router.Get("/", http.HandlerFunc(handler.Repo.Home))
+// 	router.Get("/about", http.HandlerFunc(handler.Repo.About))
+
+// 	return router
+// }
+
+// routes with chi
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	router := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handler.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handler.Repo.About))
+	router.Use(middleware.Logger)
 
-	return mux
+	router.Get("/", handler.Repo.Home)
+	router.Get("/about", handler.Repo.About)
+
+	return router
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/zdev147/hello_world_web_with_go
 
 go 1.21.1
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi v1.5.5
